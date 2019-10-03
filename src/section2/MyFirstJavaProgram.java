2e7b39df4b4bdf32f01e1fc5020285bd81ce99f0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:28:49 -0700

    Workshop robot

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,12 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot luna=new Robot();
+	luna.setSpeed(100);
+	luna.penDown();
+	for(int i=0 ;i<4; i++) {
+	luna.move(200);
+	luna.turn(90);
 	}
+}
 }
